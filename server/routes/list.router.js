b6a4f745d10d58d80e4d08c176b96207f3b9057d
Quinn Johnson <johnson.fargo87@gmail.com>
2021-01-28 11:55:22 -0600

    added post route

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
-
+router.post('/', (req, res) => {
+    const newItem = req.body;
+    const sqlText = `INSERT INTO "shopping_list" ("name" , "quantity" , "unit") VALUES ($1 , $2 , $3)`;
+    pool.query(sqlText, [newItem.name, newItem.quantity, newItem.unit])
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        });
+});
 
 module.exports = router;
