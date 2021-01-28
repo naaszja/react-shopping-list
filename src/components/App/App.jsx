5674f071371beeaa8ca390b08366ce024639157e
Quinn Johnson <johnson.fargo87@gmail.com>
2021-01-28 13:10:03 -0600

    added axios for POST

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,28 +7,42 @@ import ShoppingList from '../ShoppingList/ShoppingList.jsx'
 
 function App() {
 
-        useEffect(() => {
+    useEffect(() => {
         getShoppingList();
-      }, []); 
+    }, []);
 
-      let [shoppingList, setShoppingList] = useState([])
+    let [shoppingList, setShoppingList] = useState([])
 
-      const getShoppingList = () => { 
+    const getShoppingList = () => {
         axios({
             method: 'GET',
             url: '/list'
-        }).then((response)=> {
+        }).then((response) => {
             console.log(response.data);
             setShoppingList(response.data);
         })
     }
-
+    const addListItem = (NewItem) => {
+        console.log(NewItem);
+        // POST your data here
+        axios({
+            method: 'POST',
+            url: '/list',
+            data: {
+                name,
+                quantity,
+                unit,
+            }
+        }).then(response => {
+            getShoppingList();
+        })
+    }
     return (
         <div className="App">
             <Header />
             <main>
                 <p>Under Construction...</p>
-                <ShoppingList shoppingList={shoppingList}/>
+                <ShoppingList shoppingList={shoppingList} />
             </main>
         </div>
     );
