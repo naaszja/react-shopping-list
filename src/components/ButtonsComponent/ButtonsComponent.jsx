a1cc37515192ad298d383677db680adfb3ad77c1
Quinn Johnson <johnson.fargo87@gmail.com>
2021-01-29 07:53:00 -0600

    added swal to buttons

--- src/components/ButtonsComponent/ButtonsComponent.jsx
+++ src/components/ButtonsComponent/ButtonsComponent.jsx
@@ -1,9 +1,11 @@
-function Buttons ({resetPurchased, clearList}) {
+import "./ButtonComponent.css"
+import Swal from 'sweetalert2'
+function Buttons({ resetPurchased, clearList }) {
 
     return (
         <>
-            <button onClick = {resetPurchased}>Reset</button>
-            <button onClick = {clearList}>Clear</button>
+            <button id="reset" onClick={()=>Swal.fire({title: 'Are you sure you want to reset purchases?', text: 'All purchases will be reverted.', icon: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Yes!'}).then((result) => {if (result.value) {resetPurchased()}})}>Reset</button>
+            <button id="clear" onClick={()=>Swal.fire({title: 'Are you sure you want to clear all items?', text: 'All purchases will be lost forever.', icon: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Yes!'}).then((result) => {if (result.value) {clearList()}})}>Clear</button>
         </>
     );
 };
